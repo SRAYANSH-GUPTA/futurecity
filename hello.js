bece13e2072076824c0066544e7614bce56e8d16
SRAYANSH-GUPTA <144322123+SRAYANSH-GUPTA@users.noreply.github.com>
2024-09-08 17:38:32 +0530

    Update hello.js
    adjusting sizes

--- hello.js
+++ hello.js
@@ -2,14 +2,14 @@ function buttonenter(b)
 {
   var ele = document.querySelector(b);
   ele.style.backgroundColor = "white";
-  ele.style.height = "'50px";
-  ele.style.width = "50px";
+  ele.style.height = "70rem";
+  ele.style.width = "160rem";
 }
 function buttonleave(b)
 {
   var elem = document.querySelector(b);
   elem.style.backgroundColor = "orange";
-  elem.style.height = "30px";
-  elem.style.width = "30px";
+  elem.style.height = "62.4rem";
+  elem.style.width = "151.9rem";
 }
   
