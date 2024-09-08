750c1feca41106b43cc08a1d7202f384b81a9aa3
SRAYANSH-GUPTA <144322123+SRAYANSH-GUPTA@users.noreply.github.com>
2024-09-08 17:39:31 +0530

    Update hello.js
    size

--- hello.js
+++ hello.js
@@ -2,14 +2,14 @@ function buttonenter(b)
 {
   var ele = document.querySelector(b);
   ele.style.backgroundColor = "white";
-  ele.style.height = "70rem";
-  ele.style.width = "160rem";
+  ele.style.height = "70px";
+  ele.style.width = "160px";
 }
 function buttonleave(b)
 {
   var elem = document.querySelector(b);
   elem.style.backgroundColor = "orange";
-  elem.style.height = "62.4rem";
-  elem.style.width = "151.9rem";
+  elem.style.height = "62.4px";
+  elem.style.width = "151.9px";
 }
   
