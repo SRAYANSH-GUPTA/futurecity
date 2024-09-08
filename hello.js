dc5278f7bb35b5e052484407b205ad04fddfb0b4
SRAYANSH-GUPTA <144322123+SRAYANSH-GUPTA@users.noreply.github.com>
2024-09-08 17:29:38 +0530

    Update hello.js
    to add variable and to resolve invalid argument

--- hello.js
+++ hello.js
@@ -1,13 +1,15 @@
 function buttonenter(b)
 {
-  document.querySelector(b).style.backgroundColor = "white";
-  document.querySelector(b).style.height = "'50px";
-  document.querySelector(b).style.width = "50px";
+  var ele = document.querySelector(b);
+  ele.style.backgroundColor = "white";
+  ele.style.height = "'50px";
+  ele.style.width = "50px";
 }
 function buttonleave(b)
 {
-  document.querySelector(b).style.backgroundColor = "orange";
-  document.querySelector(b).style.height = "30px";
-  document.querySelector(b).style.width = "30px";
+  var elem = document.querySelector(b);
+  elem.style.backgroundColor = "orange";
+  elem.style.height = "30px";
+  elem.style.width = "30px";
 }
   
