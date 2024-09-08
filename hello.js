65efbeefb747434fa4dae195478095d8dee0f0ef
SRAYANSH-GUPTA <144322123+SRAYANSH-GUPTA@users.noreply.github.com>
2024-09-08 16:59:03 +0530

    Create hello.js
    creating js file to add animations

---
+++ hello.js
@@ -0,0 +1,13 @@
+function buttonenter(b)
+{
+  document.querySelector(b).style.background-color = white;
+  document.querySelector(b).style.height = 50px;
+  document.querySelector(b).style.width = 50px;
+}
+function buttonleave(b)
+{
+  document.querySelector(b).style.background-color = orangerred;
+  document.querySelector(b).style.height = 30px;
+  document.querySelector(b).style.height = 30px;
+}
+  
