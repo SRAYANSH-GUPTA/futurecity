6edc8936e77a67d2040d4d2097f69d94120fbf39
SRAYANSH-GUPTA <144322123+SRAYANSH-GUPTA@users.noreply.github.com>
2024-09-08 17:10:05 +0530

    Update hello.js
    correcting colours

--- hello.js
+++ hello.js
@@ -1,13 +1,13 @@
 function buttonenter(b)
 {
-  document.querySelector(b).style.background-color = white;
-  document.querySelector(b).style.height = 50px;
-  document.querySelector(b).style.width = 50px;
+  document.querySelector(b).style.backgroundColor = "white";
+  document.querySelector(b).style.height = "'50px";
+  document.querySelector(b).style.width = "50px";
 }
 function buttonleave(b)
 {
-  document.querySelector(b).style.background-color = orangerred;
-  document.querySelector(b).style.height = 30px;
-  document.querySelector(b).style.height = 30px;
+  document.querySelector(b).style.backgroundColor = "orange";
+  document.querySelector(b).style.height = "30px";
+  document.querySelector(b).style.width = "30px";
 }
   
