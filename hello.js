61eaf9eeae92801ee3c22bd45f8b26511e7dc644
SRAYANSH-GUPTA <144322123+SRAYANSH-GUPTA@users.noreply.github.com>
2024-09-08 18:31:31 +0530

    Update hello.js
    adding button animation

--- hello.js
+++ hello.js
@@ -1,15 +1,7 @@
-function buttonenter(b)
-{
-  var ele = document.querySelector(b);
-  ele.style.backgroundColor = "white";
-  ele.style.height = "70px";
-  ele.style.width = "160px";
-}
-function buttonleave(b)
-{
-  var elem = document.querySelector(b);
-  elem.style.backgroundColor = "orange";
-  elem.style.height = "62.4px";
-  elem.style.width = "151.9px";
-}
-  
+document.addEventListener('DOMContentLoaded', () => {
+  var buttons = document.querySelectorAll('#robo');
+  buttons.forEach(button => {
+    button.onmouseover = () => button.style.transform = 'scale(1.2)';
+    button.onmouseleave = () => button.style.transform = 'scale(1)';
+  });
+});
